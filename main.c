cfb2b28fa86f807c66f30a7b6d3c46457977b6f7
Gideon Oba <deyonoba@gmail.com>
2023-12-13 20:36:38 +0000

    Add main.c with initial test

---
+++ main.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+
+instruction_t opcode_f[] = {
+	{"pall", pall},
+	{"push", push},
+	{NULL, NULL},
+};
+
+/**
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack_queue = NULL;
+	unsigned int line_number = 1;
+	char *line = NULL, *token;
+	size_t size = 0;
+	int i;
+	FILE *file = NULL;
+
+	/* if user gives no argument or more than one argument*/
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	/* if the file failed to open argv[1] return the error*/
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&line, &size, file) != -1)
+	{
+		token = strtok(line, " \t\n");
+		if (token != NULL)
+		{
+			for (i = 0; opcode_f[i].opcode != NULL; i++)
+			{
+				if (strcmp(opcode_f[i].opcode, token) == 0)
+				{
+					opcode_f[i].f(&stack_queue, line_number);
+					break;
+				}
+			}
+			if (opcode_f[i].opcode == NULL)
+			{
+				fprintf(
+						stderr, "L%u: unknown instruction %s\n",
+						line_number, token);
+				exit(EXIT_FAILURE);
+			}
+		}
+		free(line);
+	       line_number++, line = NULL;	
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	printf("Pall Opcode found\n");
+}
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *token = NULL;
+
+	token = strtok(NULL, " \n\t");
+	printf("Push Opcode found ");
+	printf("value: %s\n", token);
+
+}
