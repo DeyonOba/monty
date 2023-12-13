f0b2ffe2e07d4ba4ea9d373b4035f8cd2335204e
Gideon Oba <deyonoba@gmail.com>
2023-12-13 20:33:20 +0000

    Update function prototype in monty.h pall and push

--- monty.h
+++ monty.h
@@ -35,4 +35,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+
 #endif
