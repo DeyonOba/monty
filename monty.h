5a24dec71358d5d679d040acd6e3e15d4d7798d0
Gideon Oba <deyonoba@gmail.com>
2023-12-14 21:17:25 +0000

    modified: monty.h with divide function prototype

--- monty.h
+++ monty.h
@@ -44,6 +44,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
 
 int is_int(const char *str);
 void free_list(stack_t *head);
