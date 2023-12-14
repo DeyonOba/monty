3d14f746fd193c9606d64e3c344b1cdee373d577
Gideon Oba <deyonoba@gmail.com>
2023-12-14 00:02:18 +0000

    refactor: push.c is betty style compliant

--- push.c
+++ push.c
@@ -1,73 +1,37 @@
 #include "main.h"
 
-/** addd_stack - This is the function that add an element be pushed
- * @stack: This is the stack of doubly linked list
- * @present: The pointer to the new node
- * Return: Nothing
- */
-
-void add_stack(stack_t **stack, stack_t *present)
-{
-	if (*stack)
-	{
-		present->next = *stack;
-		(*stack)->prev = present;
-	}
-
-	*stack = present;
-
-
 /**
- * bigi_push - This is a function that pushes an element to a atack
- * @line_num: THis is The command line number used to print error
- * Return: Nothing
+ * push - Function adds element to the top of the stack
+ *
+ * @stack: Doubly linked list containing stack elements
+ * @line_number: line number in the bytecode file
  */
-
-void bigi_push(unsigned int line_num, stack_t **stack)
+void push(stack_t **stack, unsigned int line_number)
 {
-	char *args;
+	char *value;
 	int num;
-	stack_t *present = NULL;
-	void(line_num);
+	stack_t *new_node = NULL;
 
-	if (!args)
+	value = strtok(NULL, " \t\n");
+
+	if (!value)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	num = atoi(args);
-	present = new_node(num);
+	num = atoi(value);
 
-	if (present == NULL)
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
 	{
-		fprintf(stderr, "Error: creating a new node failed\n");
+		fprintf(stderr, "memory allocation failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	add_stack(stack, present);
-}
+	new_node->n = num;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-
-
-/**
- * new_node - This is a function that crate a new stack node
- * @num: This is the value to put in the node
- * Return: A ointer to the new node
- */
-
-stack_t *new_node(int num)
-{
-	stack_t *present;
-
-	if (present == NULL)
-	{
-		return (NULL);
-	}
-
-	present->n = num;
-	present->prev = NULL;
-	present->next = NULL;
-
-	return (present);
+	*stack = new_node;
 }
