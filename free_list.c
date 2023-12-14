fec771a53298ce1cf49b3a7931d74510d9b01e77
Gideon Oba <deyonoba@gmail.com>
2023-12-14 13:21:53 +0000

    Add free_list.c free's doubly linked list

---
+++ free_list.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * free_list - Frees the doubly linked list
+ *
+ * @head: The top/head of the list
+ */
+void free_list(stack_t *head)
+{
+	stack_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
+
+
