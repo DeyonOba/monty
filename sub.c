3bc288304e693d9e6d5557593beeedf0cff9d75c
Gideon Oba <deyonoba@gmail.com>
2023-12-14 21:00:40 +0000

    refactor: sub.c code uniformity use parameter name line_number

--- sub.c
+++ sub.c
@@ -4,14 +4,14 @@
  * sub - Subtracts the top element from the second element in the stack
  *
  * @stack: The pointer to the top of the stack
- * @line_num: File line number been executed
+ * @line_number: File line number been executed
  */
 
-void sub(stack_t **stack, unsigned int line_num)
+void sub(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_num);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		free_resources();
 		exit(EXIT_FAILURE);
 	}
