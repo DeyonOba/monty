ad42135a7146520f0b207d8b3b7cddfd887035bc
Gideon Oba <deyonoba@gmail.com>
2023-12-14 17:00:25 +0000

    modified: swap.c condition checks if next is NULL

--- swap.c
+++ swap.c
@@ -11,7 +11,7 @@ void swap(stack_t **stack, unsigned int line_num)
 {
 	int temp_val;
 
-	if (!*stack || !(*stack)->prev)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
 		exit(EXIT_FAILURE);
