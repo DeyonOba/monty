e9f005dbb838a1d8a008ac6759e00ecdc0737722
Gideon Oba <deyonoba@gmail.com>
2023-12-14 22:03:38 +0000

    refactor: mul.c Error message

--- mul.c
+++ mul.c
@@ -11,7 +11,7 @@ void mul(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		free_resources();
 		exit(EXIT_FAILURE);
 	}
