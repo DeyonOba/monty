2cae204148ae8b601d702ddf5c5c3aa305dfc935
Gideon Oba <deyonoba@gmail.com>
2023-12-14 21:12:23 +0000

    Add div.c

---
+++ div.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * divide - Divide the top element from the second element in the stack
+ *
+ * @stack: The pointer to the top of the stack
+ * @line_num: File line number been executed
+ */
+
+void divide(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_resources();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_resources();
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
+}
