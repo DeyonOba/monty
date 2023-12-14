4f728b2d1c9d20e70cbf38efd9e99bc02c8a15d3
Gideon Oba <deyonoba@gmail.com>
2023-12-14 20:57:08 +0000

    refactor: sub.c Standard error output

--- sub.c
+++ sub.c
@@ -11,7 +11,7 @@ void sub(stack_t **stack, unsigned int line_num)
 {
 	if (!*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_num);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_num);
 		free_resources();
 		exit(EXIT_FAILURE);
 	}
