aab78b918bb78a9cb580a90a82b604c32af38def
Sobilo34 <bilalsolih60@gmail.com>
2023-12-15 04:58:16 -0800

    added rotr.c file rotates the stack to the right

---
+++ rotr.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * rotr - This is the fucntion that rotates the stack to the right
+ * @stack: The pointer to the head of the stack
+ * @line_number: Line number
+ */
+void rotr_handler(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+	if (*stack != NULL && (*stack)->next != NULL)
+	{
+		stack_t *last_node = *stack;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+
+		last_node->next = *stack;
+		(*stack)->prev = last_node;
+		*stack = last_node;
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
+}
+
