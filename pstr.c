bf3f223a04d561b08be6e949a381b8c0e1193d91
Sobilo34 <bilalsolih60@gmail.com>
2023-12-15 04:10:36 -0800

    modified: pstr.c changed code

--- pstr.c
+++ pstr.c
@@ -6,17 +6,18 @@
  * @line_number: The line number of command for error message printing
  * Reurn: Nothing
  */
-
 void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *element;
 	(void)line_number;
 
-	if (*stack != NULL && (*stack)->n != 0 && (*stack)->n >= 0 && (*stack)->n <= 127)
+	element = *stack;
+
+	while (element != NULL && element->n != 0 && element->n >= 0 && element->n <= 127)
 	{
-		element = *stack;
-		printf("%c", element->n);
+		printf("%c", (char)(element->n));
 		element = element->next;
 	}
+
 	printf("\n");
 }
