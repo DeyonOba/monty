6263f92dee0932aaf1f52d11600453461a0655d8
Sobilo34 <bilalsolih60@gmail.com>
2023-12-15 04:49:47 -0800

    added rotl.c file for rotating the stack

---
+++ rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * rotl - This is the function that rotates the stack to the top
+ * @stack: This is a pointer to the top of the stack
+ * @line_number: The line number of command to print error message
+ */
+
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+	if (*stack != NULL && (*stack)->next != NULL)
+	{
+		stack_t *current = *stack, *next = NULL, *prev = NULL;
+
+		while (current != NULL)
+		{
+			next = current->next;
+			current->next = prev;
+			current->prev = next;
+			prev = current;
+			current = next;
+		}
+
+		*stack = prev;
+	}
+}
