565003e9c7172e5271666cd66aac2f25895201a4
Sobilo34 <bilalsolih60@gmail.com>
2023-12-16 04:09:30 -0800

    modified: rotr.c function, implement another code for testing

--- rotr.c
+++ rotr.c
@@ -7,6 +7,28 @@
  */
 void rotr(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp = *stack;
+
+	if (*stack != NULL && (*stack)->next != NULL)
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+
+		tmp->next = *stack;
+		(*stack)->prev = tmp;
+		tmp = *stack;
+		(*stack) = tmp->prev;
+		(*stack)->prev = NULL;
+		tmp->prev = NULL;
+	}
+
+	(void)line_number;
+}
+/**
+void rotr(stack_t **stack, unsigned int line_number)
+{
 	(void)line_number;
 
 	if (*stack != NULL)
@@ -24,3 +46,4 @@ void rotr(stack_t **stack, unsigned int line_number)
 		last_node->prev = NULL;
 	}
 }
+**/
