aab41b0657f42eb6a14e2e41af6f5ddc20513e0e
Gideon Oba <deyonoba@gmail.com>
2023-12-16 21:07:01 +0000

    refactor: pstr.c now betty style compliant

--- pstr.c
+++ pstr.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * pstr - This is a function that prints string in a stack starting from the top
+ * pstr - Prints string in a stack starting from the top
  * @stack: This is the pointer to the stack
  * @line_number: The line number of command for error message printing
  * Reurn: Nothing
@@ -11,7 +11,11 @@ void pstr(stack_t **stack, unsigned int line_number)
 	stack_t *element = *stack;
 	(void)line_number;
 
-	while (element != NULL && element->n != 0 && (element->n >= 32 && element->n <= 126))
+	while (
+			element != NULL &&
+			element->n != 0 &&
+			(element->n >= 32 && element->n <= 126)
+			)
 	{
 		printf("%c", element->n);
 		element = element->next;
