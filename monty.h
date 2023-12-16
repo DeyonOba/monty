7cf39b4952120044ca3868373f41cf5bdcf57c3e
Gideon Oba <deyonoba@gmail.com>
2023-12-16 08:50:23 +0000

    modified: monty.h add function prototype rotl.c

--- monty.h
+++ monty.h
@@ -49,6 +49,7 @@ void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 
 int is_int(const char *str);
 void free_list(stack_t *head);
