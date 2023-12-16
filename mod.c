4f1447fc287bff3475eee89180103cfa9a305f1e
Gideon Oba <deyonoba@gmail.com>
2023-12-16 21:02:57 +0000

    refactor: mod.c now betty compliant

--- mod.c
+++ mod.c
@@ -4,9 +4,8 @@
  * mod - Modulus of the top element and the second element in the stack
  *
  * @stack: The pointer to the top of the stack
- * @line_num: File line number been executed
+ * @line_number: File line number been executed
  */
-
 void mod(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack || !(*stack)->next)
