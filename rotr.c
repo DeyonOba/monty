daa70e23d4dbfb35af0c511d5921508eafe7faa0
Gideon Oba <deyonoba@gmail.com>
2023-12-16 22:48:14 +0000

    fixed: rotr.c segmentation fault

--- rotr.c
+++ rotr.c
@@ -9,17 +9,18 @@
 void rotr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *last_node = *stack;
-	stack_t *top = (*stack)->next;
+	stack_t *top;
 
 	(void)line_number;
 
 	if (*stack != NULL && (*stack)->next != NULL)
 	{
+		top = (*stack)->next;
+
 		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
 		}
-
 		(*stack)->next->prev = NULL;
 		(*stack)->next = NULL;
 		(*stack)->prev = last_node;
