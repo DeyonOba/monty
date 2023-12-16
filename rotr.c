9b5e4c8f7c7718af06c6dd07c75930d7991d16e4
Sobilo34 <bilalsolih60@gmail.com>
2023-12-16 04:27:41 -0800

    modified: rotr.c, update the logic of rotating the stack

--- rotr.c
+++ rotr.c
@@ -5,28 +5,7 @@
  * @stack: The pointer to the head of the stack
  * @line_number: Line number
  */
-void rotr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp = *stack;
 
-	if (*stack != NULL && (*stack)->next != NULL)
-	{
-		while (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-		}
-
-		tmp->next = *stack;
-		(*stack)->prev = tmp;
-		tmp = *stack;
-		(*stack) = tmp->prev;
-		(*stack)->prev = NULL;
-		tmp->prev = NULL;
-	}
-
-	(void)line_number;
-}
-/**
 void rotr(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
@@ -40,9 +19,10 @@ void rotr(stack_t **stack, unsigned int line_number)
 		}
 
 		last_node->next = *stack;
-		last_node->prev->next = NULL;
-		(*stack)->prev = last_node;
-		*stack = last_node;
+		(*stack)-> = last_node;
+		last_node = *stack;
+		(*stack) = last_node-prev;
+		(*stack)->prev = NULL;
 		last_node->prev = NULL;
 	}
 }
